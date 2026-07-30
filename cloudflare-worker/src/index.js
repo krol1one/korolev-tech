c385bb0e9136691ae818f805ba0e69c060625b36
Vladislav Korolev <i@korolev-tech.ru>
2026-07-30 11:32:19 +0300

    Format lead timestamps in Moscow time

--- cloudflare-worker/src/index.js
+++ cloudflare-worker/src/index.js
@@ -160,8 +160,20 @@ function formatLeadMessage(lead) {
     "vladislavkorolev.ru",
     "",
     "🕒 Время:",
-    new Date().toISOString()
+    formatMoscowTime()
   ].join("\n");
+}
+
+function formatMoscowTime() {
+  return new Intl.DateTimeFormat("ru-RU", {
+    timeZone: "Europe/Moscow",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  }).format(new Date()) + " МСК";
 }
 
 async function sendTelegramMessage(env, text) {
